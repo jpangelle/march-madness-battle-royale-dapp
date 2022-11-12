3356bcd5c0144a8bd5557320c1b19b973ff11204
JP Angelle <jp@angelle.me>
2022-11-11 22:30:56 -0600

    write registration functions

--- contracts/contracts/Survivor.sol
+++ contracts/contracts/Survivor.sol
@@ -3,7 +3,44 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Survivor {
+contract Survivor is Ownable {
+    struct Entry {
+        string entryName;
+        bool alive;
+        string[] picks;
+    }
 
+    address[] public entryAddresses;
+    bool public isRegistrationOpen = false;
+
+    mapping(address => Entry[]) public entries;
+
+    function registerEntry(string memory _entryName) public {
+        require(isRegistrationOpen, "Registration is closed");
+        Entry memory newEntry;
+        newEntry.entryName = _entryName;
+        newEntry.alive = true;
+        entries[msg.sender].push(newEntry);
+        entryAddresses.push(msg.sender);
+    }
+
+    function resetSurvivor() public onlyOwner{
+        require(!isRegistrationOpen, "Registration must be closed in order to reset");
+        for (uint i = 0; i < entryAddresses.length; i++) {
+            delete entries[entryAddresses[i]];
+        }
+        delete entryAddresses;
+    }
+
+    function openRegistration() public onlyOwner {
+        require(!isRegistrationOpen, "Registration is already open");
+        isRegistrationOpen = true;
+    }
+
+    function closeRegistration() public onlyOwner{
+        require(isRegistrationOpen, "Registration is already closed");
+        isRegistrationOpen = false;
+    }
 }
--- contracts/test/Survivor.ts
+++ contracts/test/Survivor.ts
@@ -12,7 +12,78 @@ describe("Survivor", function () {
     return { survivor, owner, otherAccount };
   }
 
-  it("should", async function () {
-    const { survivor } = await loadFixture(deploySurvivorFixture);
+  it("should handle registration status", async function () {
+    const { survivor, otherAccount } = await loadFixture(deploySurvivorFixture);
+
+    expect(await survivor.isRegistrationOpen()).to.be.false;
+
+    await expect(
+      survivor.connect(otherAccount).openRegistration()
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+
+    await survivor.openRegistration();
+
+    await expect(survivor.openRegistration()).to.be.revertedWith(
+      "Registration is already open"
+    );
+    expect(await survivor.isRegistrationOpen()).to.be.true;
+
+    await expect(
+      survivor.connect(otherAccount).closeRegistration()
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+
+    await survivor.closeRegistration();
+    await expect(survivor.closeRegistration()).to.be.revertedWith(
+      "Registration is already closed"
+    );
+    expect(await survivor.isRegistrationOpen()).to.be.false;
+  });
+
+  it("should handle entry registration", async function () {
+    const { survivor, otherAccount } = await loadFixture(deploySurvivorFixture);
+
+    await expect(
+      survivor.connect(otherAccount).registerEntry("slamma jamma")
+    ).to.be.revertedWith("Registration is closed");
+
+    await survivor.openRegistration();
+
+    await survivor.connect(otherAccount).registerEntry("slamma jamma");
+
+    const [entryName, alive] = await survivor.entries(otherAccount.address, 0);
+
+    expect(await survivor.entryAddresses(0)).to.equal(otherAccount.address);
+    expect(entryName).to.equal("slamma jamma");
+    expect(alive).to.equal(true);
+  });
+
+  it("should reset survivor", async function () {
+    const { survivor, otherAccount } = await loadFixture(deploySurvivorFixture);
+
+    await survivor.openRegistration();
+
+    await survivor.connect(otherAccount).registerEntry("slamma jamma");
+
+    const [entryName, alive] = await survivor.entries(otherAccount.address, 0);
+
+    expect(entryName).to.equal("slamma jamma");
+    expect(alive).to.equal(true);
+
+    await expect(survivor.resetSurvivor()).to.be.revertedWith(
+      "Registration must be closed in order to reset"
+    );
+
+    await survivor.closeRegistration();
+
+    await expect(
+      survivor.connect(otherAccount).resetSurvivor()
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+
+    await survivor.resetSurvivor();
+
+    await expect(survivor.entryAddresses(0)).to.be.revertedWithoutReason();
+    await expect(
+      survivor.entries(otherAccount.address, 0)
+    ).to.be.revertedWithoutReason();
   });
 });
