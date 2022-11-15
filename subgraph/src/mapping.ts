5309d03e6c207a937344571a8bb13c49a6bb9785
JP Angelle <jp@angelle.me>
2022-11-14 21:46:38 -0600

    set up subgraph

---
+++ subgraph/src/mapping.ts
@@ -0,0 +1,99 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  BattleRoyale,
+  AdminRoleGranted,
+  BattleRoyalePoolReset,
+  ContractDeployed,
+  DaySet,
+  PickMade,
+  Registered,
+  RegistrationClosed,
+  RegistrationOpened,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  TeamsEliminated,
+  WinnerPaidout,
+} from "../generated/BattleRoyale/BattleRoyale";
+import { ExampleEntity } from "../generated/schema";
+
+export function handleAdminRoleGranted(event: AdminRoleGranted): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex());
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new ExampleEntity(event.transaction.from.toHex());
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0);
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1);
+
+  // Entity fields can be set based on event parameters
+  entity.adminAddress = event.params.adminAddress;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.ADMIN_ROLE(...)
+  // - contract.DEFAULT_ADMIN_ROLE(...)
+  // - contract.day(...)
+  // - contract.eliminatedTeams(...)
+  // - contract.getAllowance(...)
+  // - contract.getEliminatedTeams(...)
+  // - contract.getPoolEntries(...)
+  // - contract.getPoolEntryPicks(...)
+  // - contract.getRoleAdmin(...)
+  // - contract.hasRole(...)
+  // - contract.isEntryEliminated(...)
+  // - contract.isRegistrationOpen(...)
+  // - contract.poolEntries(...)
+  // - contract.poolEntryAddresses(...)
+  // - contract.supportsInterface(...)
+}
+
+export function handleBattleRoyalePoolReset(
+  event: BattleRoyalePoolReset
+): void {}
+
+export function handleContractDeployed(event: ContractDeployed): void {}
+
+export function handleDaySet(event: DaySet): void {}
+
+export function handlePickMade(event: PickMade): void {}
+
+export function handleRegistered(event: Registered): void {}
+
+export function handleRegistrationClosed(event: RegistrationClosed): void {}
+
+export function handleRegistrationOpened(event: RegistrationOpened): void {}
+
+export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
+
+export function handleRoleGranted(event: RoleGranted): void {}
+
+export function handleRoleRevoked(event: RoleRevoked): void {}
+
+export function handleTeamsEliminated(event: TeamsEliminated): void {}
+
+export function handleWinnerPaidout(event: WinnerPaidout): void {}
