be67887942dbc05069f0d7729aa68dad46a0b82c
JP Angelle <jp@angelle.me>
2022-12-08 20:19:54 -0600

    subgraph stuff

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -13,19 +13,20 @@ import {
   RoleGranted,
   RoleRevoked,
   TeamsEliminated,
+  VersionIncremented,
   WinnerPaidout,
 } from "../generated/BattleRoyale/BattleRoyale";
-import { ExampleEntity } from "../generated/schema";
+import { Admin } from "../generated/schema";
 
 export function handleAdminRoleGranted(event: AdminRoleGranted): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex());
+  let entity = Admin.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex());
+    entity = new Admin(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0);
@@ -39,37 +40,6 @@ export function handleAdminRoleGranted(event: AdminRoleGranted): void {
 
   // Entities can be written to the store with `.save()`
   entity.save();
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ADMIN_ROLE(...)
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.day(...)
-  // - contract.eliminatedTeams(...)
-  // - contract.getAllowance(...)
-  // - contract.getEliminatedTeams(...)
-  // - contract.getPoolEntries(...)
-  // - contract.getPoolEntryPicks(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.isEntryEliminated(...)
-  // - contract.isRegistrationOpen(...)
-  // - contract.poolEntries(...)
-  // - contract.poolEntryAddresses(...)
-  // - contract.supportsInterface(...)
 }
 
 export function handleBattleRoyalePoolReset(
@@ -95,5 +65,7 @@ export function handleRoleGranted(event: RoleGranted): void {}
 export function handleRoleRevoked(event: RoleRevoked): void {}
 
 export function handleTeamsEliminated(event: TeamsEliminated): void {}
+
+export function handleVersionIncremented(event: VersionIncremented): void {}
 
 export function handleWinnerPaidout(event: WinnerPaidout): void {}
